32751002aaae4cc55448a272cb56ff52d45a1754
出島璃空 <>
2021-01-22 14:56:38 +0900

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,17 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttershare/models/user.dart';
-import 'package:fluttershare/pages/activity_feed.dart';
 import 'package:fluttershare/pages/create_account.dart';
+import 'package:fluttershare/pages/timeline.dart';
+import 'package:fluttershare/pages/activity_feed.dart';
 import 'package:fluttershare/pages/profile.dart';
 import 'package:fluttershare/pages/search.dart';
-import 'package:fluttershare/pages/timeline.dart';
 import 'package:fluttershare/pages/upload.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:fluttershare/models/user.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final StorageReference storageRef = FirebaseStorage.instance.ref();
@@ -27,28 +29,27 @@ class _HomeState extends State<Home> {
   bool isAuth = false;
   PageController pageController;
   int pageIndex = 0;
+  User currentUser;
 
   @override
   void initState() {
     super.initState();
     pageController = PageController();
-    // Detects when user signed in
     googleSignIn.onCurrentUserChanged.listen((account) {
       handleSignIn(account);
     }, onError: (err) {
-      print('Error signing in: $err');
+      print("Error signing in: $err");
     });
-    // Reauthenticate user when app is opened
+
     googleSignIn.signInSilently(suppressErrors: false).then((account) {
       handleSignIn(account);
-    }).catchError((err) {
-      print('Error signing in: $err');
     });
   }
 
   handleSignIn(GoogleSignInAccount account) {
     if (account != null) {
       createUserInFirestore();
+      print("User signed in!: $account");
       setState(() {
         isAuth = true;
       });
@@ -60,16 +61,13 @@ class _HomeState extends State<Home> {
   }
 
   createUserInFirestore() async {
-    // 1) check if user exists in users collection in database (according to their id)
     final GoogleSignInAccount user = googleSignIn.currentUser;
-    DocumentSnapshot doc = await usersRef.document(user.id).get();
+    print(user);
+    final DocumentSnapshot doc = await usersRef.document(user.id).get();
 
     if (!doc.exists) {
-      // 2) if the user doesn't exist, then we want to take them to the create account page
       final username = await Navigator.push(
           context, MaterialPageRoute(builder: (context) => CreateAccount()));
-
-      // 3) get username from create account, use it to make new user document in users collection
       usersRef.document(user.id).setData({
         "id": user.id,
         "username": username,
@@ -77,12 +75,9 @@ class _HomeState extends State<Home> {
         "email": user.email,
         "displayName": user.displayName,
         "bio": "",
-        "timestamp": timestamp
+        "timestamp": timestamp,
       });
-
-      doc = await usersRef.document(user.id).get();
     }
-
     currentUser = User.fromDocument(doc);
   }
 
@@ -107,98 +102,97 @@ class _HomeState extends State<Home> {
   }
 
   onTap(int pageIndex) {
-    pageController.animateToPage(
-      pageIndex,
-      duration: Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
+    pageController.animateToPage(pageIndex,
+        duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
   }
 
-  Scaffold buildAuthScreen() {
+  buildAuthScreen() {
     return Scaffold(
       body: PageView(
         children: <Widget>[
-          // Timeline(),
           RaisedButton(
-            child: Text('Logout'),
+            child: Text("Logout"),
             onPressed: logout,
           ),
+          // Timeline(),
           ActivityFeed(),
-          Upload(currentUser: currentUser),
+          Upload(
+            currentUser: currentUser,
+          ),
           Search(),
-          Profile(profileId: currentUser?.id),
+          Profile(profileId: currentUser?.id)
         ],
         controller: pageController,
         onPageChanged: onPageChanged,
         physics: NeverScrollableScrollPhysics(),
       ),
       bottomNavigationBar: CupertinoTabBar(
-          currentIndex: pageIndex,
-          onTap: onTap,
-          activeColor: Theme.of(context).primaryColor,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.whatshot)),
-            BottomNavigationBarItem(icon: Icon(Icons.notifications_active)),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.photo_camera,
-                size: 35.0,
-              ),
+        currentIndex: pageIndex,
+        onTap: onTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_active),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.photo_camera,
+              size: 35.0,
             ),
-            BottomNavigationBarItem(icon: Icon(Icons.search)),
-            BottomNavigationBarItem(icon: Icon(Icons.account_circle)),
-          ]),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
     );
-    // return RaisedButton(
-    //   child: Text('Logout'),
-    //   onPressed: logout,
-    // );
+    // return RaisedButton(child: Text("logout"), onPressed: logout);
   }
 
   Scaffold buildUnAuthScreen() {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
             colors: [
-              Theme.of(context).accentColor,
               Theme.of(context).primaryColor,
+              Theme.of(context).accentColor
             ],
-          ),
-        ),
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'FlutterShare',
-              style: TextStyle(
-                fontFamily: "Signatra",
-                fontSize: 90.0,
-                color: Colors.white,
+            begin: Alignment.topRight,
+            end: Alignment.topLeft,
+          )),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Fluttershare",
+                style: TextStyle(
+                    fontFamily: "Signatra",
+                    fontSize: 90.0,
+                    color: Colors.white),
               ),
-            ),
-            GestureDetector(
-              onTap: login,
-              child: Container(
-                width: 260.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/google_signin_button.png',
-                    ),
-                    fit: BoxFit.cover,
-                  ),
+              GestureDetector(
+                onTap: () => login(),
+                child: Container(
+                  width: 260.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/google_signin_button.png"),
+                          fit: BoxFit.cover)),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+              )
+            ],
+          )),
     );
   }
 
